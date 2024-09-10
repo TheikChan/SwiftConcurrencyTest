a8663fd9554194bc288d690bdfbb524c99e3cf41
Theik Chan <theikchan.tc@gmail.com>
2024-09-11 00:15:30 +0700

    Initial Commit

---
+++ SwiftConcurrencyTest/SwiftConcurrencyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyTestApp.swift
+//  SwiftConcurrencyTest
+//
+//  Created by Theik Chan on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
